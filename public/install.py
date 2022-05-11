5d635db4b9170975fe6de258229b9bc41fcf0229
NotTimIsReal <alastairrintumathew@gmail.com>
2022-05-11 15:36:14 +1000

    edit

--- public/install.py
+++ public/install.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 import requests
+import sys
 os.chdir("/tmp")
 res=requests.get("https://ferment.tk/install.sh")
 #get content
@@ -8,4 +10,4 @@ with open("install.sh", "wb") as f:
     f.write(content)
     f.close()
 #run install.sh
-os.system("sh install.sh")
+subprocess.call(["sh", "./install.sh"], stdin=sys.stdin)
