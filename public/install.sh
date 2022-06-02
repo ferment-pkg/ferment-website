ad7d779e9cd623994812eeef2b903c07754c5a99
NotTimIsReal <alastairrintumathew@gmail.com>
2022-06-02 21:45:36 +1000

    new install script

--- public/install.sh
+++ public/install.sh
@@ -35,8 +35,10 @@ function run(){
     fi
   fi
   echo "Cloning The Package Manager..."
+  sudo mkdir /usr/localbin
   sudo mkdir -p /usr/local/ferment
   sudo chgrp admin /usr/local/ferment
+  sudo chgrp admin /usr/local/bin
   sudo chmod -R 2775 /usr/local/ferment
   git clone https://github.com/ferment-pkg/ferment /usr/local/ferment/
   #check is zshrc is installed
