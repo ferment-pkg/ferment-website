1f64f85ef3f501d4ad9ac62dfa39588756a89e5a
NotTimIsReal <84319818+NotTimIsReal@users.noreply.github.com>
2022-06-06 20:41:12 +1000

    Update install.sh

--- public/install.sh
+++ public/install.sh
@@ -37,9 +37,9 @@ function run(){
   echo "Cloning The Package Manager..."
   sudo mkdir /usr/local/bin
   sudo mkdir -p /usr/local/ferment
-  sudo chgrp staff /usr/local/ferment
-  sudo chgrp staff /usr/local/bin
   sudo chmod -R 2775 /usr/local/ferment
+  sudo chmod -R $USER:admin /usr/local/ferment
+  sudo chmod -R $USER:admin /usr/local/bin
   git clone  --recurse-submodules -j8 https://github.com/ferment-pkg/ferment /usr/local/ferment/
   #check is zshrc is installed
   if test -f "$HOME/.zshrc"
